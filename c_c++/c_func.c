8fb4befc350f319a9650166a6795cd407b7fa067
iourigordon <iouri.gordon@gmail.com>
2017-09-29 18:10:24 -0700

    testing webhook

--- c_c++/c_func.c
+++ c_c++/c_func.c
@@ -12,6 +12,7 @@ extern "C" {
         structure.sum();
         printf("structure.c = %d; structure.a = %d; structure.b = %d\n",structure.c,structure.a,structure.b);
         printf("structure2.c = %d; structure2.a = %d; structure2.b = %d\n",structure2.c,structure2.a,structure2.b);
+        printf("hello\n");
     }
 
 
